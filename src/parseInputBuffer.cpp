07495f5a7195883751aa77eecaa9de8be2cc5a02
Jon Leithe <jon@jonleithe.no>
2024-05-26 10:17:51 +0200

    Working on parser...

--- src/parseInputBuffer.cpp
+++ src/parseInputBuffer.cpp
@@ -16,9 +16,9 @@
 
 void fileoperations::parseInputBuffer(void)
 {
-	int				i;
+	//size_t			i;
 
-	unsigned int	sessionTypeIndex, antennaIndex, searchIndex;
+	unsigned int	/*sessionTypeIndex,*/ antennaIndex, searchIndex;
 
 	string			searchTerm = "Ns";
 	string			htmlToken;
@@ -43,10 +43,31 @@ void fileoperations::parseInputBuffer(void)
 
 	// First we search for the antenna name, returning an index for that position
 	antennaIndex = fo_inputBuffer.find(searchTerm);
+	cout << "parseInputBuffer(): antennaIndex: " << antennaIndex << endl;
 
 	// Then we need to back up making sure the particular antenna is not removed from the session
 	// This can be done by reversing a bit, and do a search for the phrase "station-id removed"
+	// Let's keep the antennaIndex where it's at, and assign teh value of antennaInde to searchIndex,
+	// using searchIndex to back up the string and search for the new search term
 	searchIndex = antennaIndex;
+	searchTerm = "<li class=\"station-id ";
+	searchIndex -= searchTerm.length();
+
+	//i = string::npos;
+	while( (fo_inputBuffer.find(searchTerm, searchIndex)) == string::npos){
+		// Means were still looking for the search term...
+		searchIndex--;
+		cout << "searchIndex: " << searchIndex << endl;
+	}
+	cout << "Search term " << searchTerm << "found at " << searchIndex << endl;
+
+	/*
+	i = fo_inputBuffer.find(searchTerm);
+	if(i != std::string::npos){
+		// means we found it
+		cout << "Search term found" << endl;
+	}
+	*/
 
 
 
